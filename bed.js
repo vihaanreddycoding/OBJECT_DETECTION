ca04c6cbf667352adceae251202b391a9fad0a88
vihaanreddycoding <99722949+vihaanreddycoding@users.noreply.github.com>
2022-08-27 09:14:20 +0530

    Add files via upload

---
+++ bed.js
@@ -0,0 +1,34 @@
+img = " "
+
+function preload() {
+img = loadImage('download.jpg');
+}
+
+function setup() {
+    canvas = createCanvas(640, 420);
+    canvas.center();
+    objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("status").innerHTML = "Status : Detecting Objects";
+}
+
+function draw() {
+    image(img, 0, 0, 640, 420);
+    fill("#FF0000");
+    text("BED", 45, 75);
+    noFill();
+    stroke("#FF0000")
+    rect( );
+}
+
+function modelLoaded() {
+    console.log("Model Loaded!")
+    status = true;
+    objectDetector.detect(img, gotResult);
+}
+
+function gotResult(error, results) {
+    if(error) {
+        console.log(error);
+    }
+    console.log(results);
+}
